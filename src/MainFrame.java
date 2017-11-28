2f8857d2c79a4b5db2edee6840c084d9e15f8731
USER <1050201057@lice.asia.edu.tw>
2017-11-28 12:50:21 +0800

    123

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -10,8 +10,12 @@ public class MainFrame extends JFrame {
     private JButton bstop = new JButton("Stop");
     private JButton bexit = new JButton("Exit");
     private JButton bsend = new JButton("Send");
-
+    private JTextArea jta  = new JTextArea();
+    private JLabel jlbport = new JLabel("Port");
+    private JLabel jlbIP = new JLabel("Ueser IP");
+    private JTextField jtf = new JTextField();
     private void init(){
-
+    cp = this.getContentPane();
+    
     }
 }
