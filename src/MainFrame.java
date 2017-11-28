6e83038e28e4c8f820e4a50bd408ffd5c69b883e
USER <1050201057@lice.asia.edu.tw>
2017-11-28 11:56:02 +0800

    123

---
+++ src/MainFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    MainFrame(){
+    init();
+    }
+    private Container cp;
+    private JButton bstart = new JButton("Start");
+    private JButton bstop = new JButton("Stop");
+    private JButton bexit = new JButton("Exit");
+    private JButton bsend = new JButton("Send");
+
+    private void init(){
+
+    }
+}
